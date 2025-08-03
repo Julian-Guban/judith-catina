c96c105bdb99626633f9fb6eb9783d0b6cd65612
Julian Guban <jpguban04@gmail.com>
2025-08-03 21:32:07 +0800

    added exam

--- src/pages/Exam.js
+++ src/pages/Exam.js
@@ -1,7 +1,17 @@
 import React from "react";
 import tos from "../assets/Exam/tos.png";
+import file from "../assets/Exam/EXAMINATION_FOR_COOKERY.pdf";
+import file1 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0001.jpg";
+import file2 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0002.jpg";
+import file3 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0003.jpg";
+import file4 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0004.jpg";
+import file5 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0005.jpg";
+import file6 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0006.jpg";
+import file7 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0007.jpg";
+import file8 from "../assets/Exam/EXAMINATION_FOR_COOKERY_page-0008.jpg";
 
 function Exam() {
+  const images = [file1, file2, file3, file4, file5, file6, file7, file8];
   return (
     <div id="Exam">
       <div
@@ -15,7 +25,22 @@ function Exam() {
           <h1 className="text-muted">TOS</h1>
           <p className="text-muted">- Table of Specification</p>
         </div>
-        {/* <button
+      </div>
+      <div className="flex-center">
+            <img src={tos} width={"100%"} alt="tos" height={"100%"}/>
+      </div>
+      <div
+        className="w-100 flex-row center p-2"
+        style={{ justifyContent: "space-between" }}
+      >
+        <div
+          className="flex-column"
+          style={{ alignItems: "flex-start", gap: "0px" }}
+        >
+          <h1 className="text-muted">Exam</h1>
+          <p className="text-muted">- 50-item Multiple Choice Exam</p>
+        </div>
+        <button
           className="btn btn-secondary"
           style={{ height: "fit-content !important" }}
         >
@@ -25,12 +50,14 @@ function Exam() {
             rel="noreferrer"
             style={{ textDecoration: "none" }}
           >
-            Download DLP
+            Download Exam
           </a>
-        </button> */}
+        </button>
       </div>
-      <div className="flex-center">
-            <img src={tos} width={"100%"} height={"100%"}/>
+       <div className="x-auto" style={{width: "inherit", display: "flex", flexDirection: "row", gap: "5px", alignItems: "flex-start"}}>
+        {images.map((image, idx) => (
+          <img src={image} key={idx} height={"550px"} alt="Loading... " />
+        ))}
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
-import img from "../assets/My-E-Portfolio.png";
+import img from "../assets/0000e95b-77f1-44c9-99f1-0735ef82c6f7.jfif";
 
 function Home() {
   return (
     <div id="Home">
-      <div className="w-100 flex-center">
+      <div className="flex-center">
         <img src={img} alt="My E-Portfolio" height={"100%"} />
       </div>
       <div className="intro bg-panel">
-          <h2>This is The E-Portfolio of </h2>
-          <a href="https://www.facebook.com/judith.catina.3" target="_blank" rel="noreferrer">Judith Catina</a>
-          <p>from: </p>
-          <p>BTVTEdICT-CP-3D</p>
+          <h2>Welcome to My E-Portfolio!</h2>
+          <a href="https://www.facebook.com/judith.catina.3" target="_blank" rel="noreferrer">by: Judith Catina</a>
+          <p>from: BTVTEdICT-CP-3D</p>
       </div>
     </div>
   );
